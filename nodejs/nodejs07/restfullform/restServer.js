1b75ff70fa7a2c5263e056455cae3dcf86483408
eumcloud <livelch012@naver.com>
2021-11-24 08:22:10 +0000

    restfull 예시

--- nodejs/nodejs07/restfullform/restServer.js
+++ nodejs/nodejs07/restfullform/restServer.js
@@ -1,84 +1,88 @@
-const http = require("http");
-const fs = require("fs").promises;
+const http = require('http');
+const fs = require('fs').promises;
 
-let host = "127.0.0.1";
-let port = 3000;
+const host = '127.0.0.1';
+const port = 3000
 
 const users = {};
-var server = http.createServer(async (req, res)=>{
-    try{
-        if(req.method ==="GET"){
-            if(req.url === "/"){
-                const data = await fs.readFile("./restFront.html");
-                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+
+var server = http.createServer(async(req, res) => {
+    try {
+        if(req.method === 'GET') {
+            if(req.url === '/') {
+                const data = await fs.readFile('./restFront.html');
+                res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
                 return res.end(data);
-            }else if(req.url ==="/about"){
-                const data = await fs.readFile("./about.html");
-                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+            }
+            else if(req.url === '/about') {
+                const data = await fs.readFile('./about.html');
+                res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
                 return res.end(data);
-            }else if(req.url ==="/users"){
-                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
             }
-            try{
-                const data = await fs.readFile(`.${req.url}`);
-                return res.end(data);
-            }catch(err){
-                res.writeHead(404, {"Content-Type": "text/html; charset=utf-8"});
-                return res.end(err.message);
-            }
-        }
-        else if(req.method ==="POST"){
-            if(req.url === "/user"){
-                let body = "";
-                // request body => stream
-                req.on("data", (data)=>{
-                    body += data;
-                });
-                // body after download execute
-                req.on("data", (data)=>{
-                    console.log(`POST body :`, body);
-                    const {name} = JSON.parse(body);
-                    const id = Date.now();
-                    user[id] = name;
-                    res.writeHead(201, {"Content-Type": "text/html; charset=utf-8"});
-                    res.end("ok");
-                });
-            }
-        }
-        else if(req.method === "PUT"){
-            if(req.url.startsWith("/user/")){
-                const key = req.url.split("/")[2];//세번째거 가져옴
-                let body = "";
-                req.on("data", (data)=>{
-                    body += data;
-                });
-                return req.on("end", ()=>{
-                    console.log("PUT body:", body);
-                    users[key] = JSON.parse(body).name;
-                    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-                    return res.end("ok");
-                });
+            else if(req.url === '/users') {
+                res.writeHead(200, {'Content-Type' : 'application/json; charset=utf-8'});
+                return res.end(JSON.stringify(users));
             }
 
+            try {
+                const data = await fs.readFile(`.${req.url}`);
+                return res.end(data); 
+            } catch(err) {               
+                // res.writeHead(404, {'Content-Type' : 'text/html; charset=utf-8'});
+                // return res.end(err.message);
+            }
         }
-        else if(req.method === "DELETE"){
-            if(req.url.startsWith("/user")){
-                const key = req.url.split("/")[2];
+        else if(req.method === 'POST') {
+            if(req.url === '/user/') {                            // '/user 수정
+                let body = '';
+                // request body -> stream
+                req.on('data', (data) => {
+                    body += data;
+                });
+                // body after down execute 
+                return req.on('end', () => {            // (data) 수정
+                    console.log('POST body:', body);
+                    const { name } = JSON.parse(body);
+                    const id = Date.now();
+                    users[id] = name;
+                    res.writeHead(201, {'Content-Type' : 'text/html; charset=utf-8'});
+                    res.end('ok');
+                });
+            }
+        }
+        else if(req.method === 'PUT') {
+            if(req.url.startsWith('/user/')) {
+                const key = req.url.split('/')[2];
+                let body = '';
+                req.on('data', (data) => {
+                    body += data;
+                });
+                return req.on('end', () => {
+                    console.log('PUT body:', body);
+                    users[key] = JSON.parse(body).name;
+                    res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+                    return res.end('ok');
+                });
+            };
+        }
+        else if(req.method === 'DELETE') {
+            if(req.url.startsWith('/user/')) {               // '/user 수정
+                const key = req.url.split('/')[2];
                 delete users[key];
-                res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-                return res.end("ok");
+                res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+                return res.end('ok');
             }
         }
         res.writeHead(404);
-        return res.end("NOT FOUND");
-    }catch(err){
+        return res.end('NOT FOUND');
+    }
+    catch(err) {
         console.error(err);
-        res.writeHead(500, {"Content-Type": "text/html; charset=utf-8"});
+        res.writeHead(500, {'Content-Type' : 'text/html; charset=utf-8'});
         res.end(err.message);
     }
-
 });
 
-server.listen(port, host, ()=>{
-    console.log(`Server running at http://${host}:${port}`);
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
 });
