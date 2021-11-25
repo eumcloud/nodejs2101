3c2c4a0ff66abdd1481356de995da3e60d129092
eumcloud <livelch012@naver.com>
2021-11-25 01:13:45 +0000

    빌드2

--- nodejs/nodejs09/app.js
+++ nodejs/nodejs09/app.js
@@ -7,6 +7,10 @@ const host = "127.0.0.1";
 app.get("/:id", (req, res)=>{//빌드 라고한다.
     res.send("This id is : " + req.params.id);
 });
+app.get("/hello/:name/:id", (req, res)=>{
+    res.send("ID : "+ req.params.id + " name " + req.params.name);
+});
+
 
 
 app.listen(port, host, ()=>{
